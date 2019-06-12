7e866f835ac48df253aac041b2e8ab771d499251
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2019-06-12 16:36:18 +0530

    Added Script to build in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+var path = require('path');
+
 
 module.exports = {
   entry: './client/index.js',
   output: {
-    path: '/',
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js'
   },
   module: {
