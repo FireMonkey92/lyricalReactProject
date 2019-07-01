cb30bebd5f37e6b1ce3fde581a8750320778280f
Tanesh <35836662+FireMonkey92@users.noreply.github.com>
2019-07-01 23:55:42 +0530

    Some changes, Added like mutation

--- client/components/LyricsListRander.js
+++ client/components/LyricsListRander.js
@@ -1,6 +1,17 @@
 import React, { Component } from 'react'
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
-export default class LyricsListRander extends Component {
+class LyricsListRander extends Component {
+
+    onLikeClick(id){
+        this.props.mutate({
+            variables : {id}
+        })
+    }
+    onDeleteClick(id){
+        this.props.mutate({variables : {id}});
+    }
     renderSongsLyrics({ lyrics }) {
         return lyrics.map((item) => {
             return (
@@ -8,16 +19,18 @@ export default class LyricsListRander extends Component {
                     <div className="s-title">
                         <span>{item.content}</span>
                     </div>
-                    <div className="list-operation" style={{ textAlign: "right" }}>
+                    <div className="v-box" style={{ textAlign: "right" }}>
+                       
+                        <i onClick={()=>this.onLikeClick(item.id)} style={{ cursor: "pointer" }} className="material-icons">thumb_up_alt</i>
+                        {/* <i onClick={()=>this.onDeleteClick(item.id)} style={{ cursor: "pointer" }} className="material-icons">delete_forever</i> */}
                         <span className="op-item" style={{ marginRight: "5px" }}> {item.likes} </span>
-                        <i style={{ cursor: "pointer" }} className="material-icons">thumb_up_alt</i>
                     </div>
                 </div>
             )
         })
     }
     render() {
-        if (this.props.lyrics.length === 0) { return (<h5>There is no lyrics added for this song, Please add one..!!</h5>) }
+        if (this.props.lyrics.length === 0) { return (<div className="Warning"><i style={{fontSizez: 50}} className="material-icons">warning</i><h5>There is no lyrics added for this song, Please add one..!!</h5></div>) }
         return (
             <div>
                 <div style={{ marginLeft: '5px' }}>Lyrics for {this.props.title}</div>
@@ -28,3 +41,20 @@ export default class LyricsListRander extends Component {
         )
     }
 }
+
+const mutation = gql`
+mutation likeSongLyric($id : ID!){
+    likeLyric(id : $id){
+        id
+      likes
+    }
+}`;
+
+const editMutation= gql`mutation editLyric($id : ID!){
+    editLyric(id : $id){
+          id
+        likes
+      }
+    }`;
+
+export default  graphql(mutation)(LyricsListRander);
